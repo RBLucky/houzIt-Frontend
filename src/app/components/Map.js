e4e1e222415dc90be386939261700814cb4a34eb
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-04 20:09:21 +0200

    initialized google maps api

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,3 +1,8 @@
+"use client"
+
+import { useState, useCallback, memo } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+
 const Map = () => {
     return (
         <></>
