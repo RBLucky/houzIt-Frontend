e4ed9abce9f4692a29cf1b753349c194e531b3f2
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-04 18:11:01 +0200

    graphql query

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,61 @@
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
 import Map from "./components/Map";
 import Card from "./components/Card";
 
+const getProperties = async () => {
+  const HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT;
+  if (!HYGRAPH_ENDPOINT) {
+    throw new Error("HYGRAPH_ENDPOINT is not set");
+  }
+
+  const response = await fetch(HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query MyQuery {
+          properties {
+            beds
+            description
+            images {
+              fileName
+              url
+            }
+            location {
+              latitude
+              longitude
+            }
+            name
+            rentalPrice
+            slug
+            id
+          }
+        }`,
+    }),
+  });
+};
+
 const Home = () => {
   return (
     <>
-      <NavBar/>
-      <SearchBar/>
+      <NavBar />
+      <SearchBar />
       <main>
         <article>
-          <Map/>
+          <Map />
         </article>
         <article className="listings">
-          <h2>
-            Rental Listings
-          </h2>
+          <h2>Rental Listings</h2>
           <div className="card-container">
-            <Card/>
+            <Card />
           </div>
         </article>
       </main>
     </>
   );
-}
-
+};
 
 export default Home;
