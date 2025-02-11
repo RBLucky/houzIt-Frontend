fc9ae30fc77968575b9e0b5e18c5dc59d1ab70e0
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-11 02:00:50 +0200

    added Hackathon... text in navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -2,6 +2,9 @@ const NavBar = () => {
     return (
         <nav>
             {/* houz'It logo*/}
+            <center style={{ marginBottom: '0rem', fontSize: '1.5rem', fontWeight: 'bold' }}>
+                HACKATHON PROJECT IN PROGRESS... 🚧
+            </center>
         </nav>
     );
 }
